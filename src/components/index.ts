07dd761bdb5fdc26797e4bafe749f370fed4eb9f
Leandro Tamola <leatamola@gmail.com>
2023-06-07 23:12:45 -0300

    added Counter component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Card } from './Card/Card';
+export { Counter } from './Counter/Counter';
 export { OrderBook } from './OrderBook/OrderBook';
 export { Switcher } from './Switcher/Switcher';
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
-import { ActivityIndicator, StyleSheet } from 'react-native';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Card, Switcher } from '@src/components';
+import { Card, Counter, Switcher } from '@src/components';
 import tailwind from 'twrnc';
 import { Text } from '@src/components/Text/Text';
 import { HomeViewModel } from './HomeViewModel';
@@ -23,13 +23,16 @@ const Home: FC = () => {
     <SafeAreaView style={styles.container} edges={['top']}>
       <Text style={styles.title}>SOL * USDT</Text>
       <Card style={styles.card}>
-        <Text style={styles.cardTitle}>Hello</Text>
         {getOrderBook.isLoading && !getOrderBook.data ? (
           <ActivityIndicator />
         ) : (
           <>
             <Switcher containerStyle={styles.switcher} options={SECTIONS} onPress={onTypePress} />
             <OrderBook data={getOrderBook.data!} containerStyle={styles.orderBook} />
+            <View style={styles.limitPriceContainer}>
+              <Text>Limit Price (USDT)</Text>
+              <Counter />
+            </View>
           </>
         )}
       </Card>
@@ -42,8 +45,8 @@ export { Home };
 const styles = StyleSheet.create({
   container: tailwind`flex-1 bg-black`,
   title: tailwind`text-2xl font-500 pl-4 mb-4`,
-  card: tailwind`px-4`,
-  cardTitle: tailwind`my-8`,
+  card: tailwind`px-4 py-6`,
   switcher: tailwind`mb-4 w-1/3`,
   orderBook: tailwind``,
+  limitPriceContainer: tailwind`mt-4 flex-row justify-between items-center`,
 });
